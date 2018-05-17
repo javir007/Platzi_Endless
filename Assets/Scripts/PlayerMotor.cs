f673759b798e544143e385b67aa46c2f3808db61
Javir <javir.urro01@gmail.com>
2018-05-17 14:53:14 -0500

    Cambio de Estados

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -14,16 +14,28 @@ public class PlayerMotor : MonoBehaviour {
     private float speed = 7f;
     private int lane = 1;
 
+    private bool isRunning = false;
+
 
     private CharacterController controller;
+    private Animator anim;
+
+
 
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(!isRunning){
+            return;
+        }
+
         if(Input.GetKeyDown(KeyCode.LeftArrow)){
             ChangeLane(false);
             
@@ -43,6 +55,8 @@ public class PlayerMotor : MonoBehaviour {
 
         moveTarget.x = (targetPosition - transform.position).normalized.x * speed;
         bool isGrounded = IsGrounded();
+        anim.SetBool("isGrounded", isGrounded);
+
 
         if (isGrounded)
         {
@@ -52,6 +66,7 @@ public class PlayerMotor : MonoBehaviour {
             {
 
                 verticalVelocity = jump;
+                anim.SetTrigger("Jump");
 
             }
         }
@@ -67,6 +82,7 @@ public class PlayerMotor : MonoBehaviour {
 
         //Movimiento
         controller.Move(moveTarget * Time.deltaTime);
+        anim.SetFloat("Speed", controller.velocity.z);
 
         Vector3 dir = controller.velocity;
         dir.y = 0;
@@ -77,6 +93,10 @@ public class PlayerMotor : MonoBehaviour {
 
 
 	}
+
+    public void StartRun(){
+        isRunning = true;
+    }
 
     void ChangeLane(bool isRight){
         lane += (isRight) ? 1 : -1;
