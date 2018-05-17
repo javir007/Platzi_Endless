cb2649c92dc6ecc8cbeadedfe0e64ecc41d3764a
Javir <javir.urro01@gmail.com>
2018-05-17 15:08:43 -0500

    Seguimiento de camara

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -109,4 +109,18 @@ public class PlayerMotor : MonoBehaviour {
         Ray groundRay = new Ray(new Vector3(controller.bounds.center.x, (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f, controller.bounds.center.z), Vector3.down);
         return Physics.Raycast(groundRay, 0.2f + 0.1f);
     }
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+        if(hit.gameObject.CompareTag("Obstacle")){
+            //Morir el personaje
+            Crash();
+        }
+	}
+
+    void Crash(){
+        anim.SetTrigger("Death");
+        isRunning = false;
+
+    }
 }
