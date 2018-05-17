22bf089d0ef04429a841da68ce73d3a6de1ace9c
Javir <javir.urro01@gmail.com>
2018-05-17 18:58:39 -0500

    LevelManager parte 2

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,7 +9,7 @@ public class LevelManager : MonoBehaviour {
     public List<GameObject> tiles = new List<GameObject>();
     private Transform playerTransform;
     private float spawnZ = 0.0f;
-    private float titleLength = 6f;
+    private float titleLength = 5.9f;
     private int amnTiles = 20;
     private float safeZone = 15.0f;
     private int lastTileIndex;
@@ -18,18 +18,30 @@ public class LevelManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        activeTiles = new List<GameObject>();
+        for (int i = 0; i < amnTiles; i++){
+            if(i<2){
+                SpawnTile(0);
+            }else{
+                SpawnTile();
+            }
+
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(playerTransform.position.z - safeZone > (spawnZ - amnTiles * titleLength)){
+            SpawnTile();
+            DeleteTile();
+        }
 	}
 
     void SpawnTile(int prefabIndex = -1){
         GameObject go;
         if(prefabIndex == -1){
-            go = Instantiate(tiles[0]) as GameObject;
+            go = Instantiate(tiles[RandomTileIndex()]) as GameObject;
         }else{
             go = Instantiate(tiles[prefabIndex]) as GameObject;
         }
@@ -37,6 +49,24 @@ public class LevelManager : MonoBehaviour {
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += titleLength;
         activeTiles.Add(go);
+
+    }
+
+   void DeleteTile(){
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int RandomTileIndex(){
+        if(tiles.Count <= 1){
+            return 0;
+        }
+        int randomIndex = lastTileIndex;
+        while(randomIndex == lastTileIndex){
+            randomIndex = Random.Range(0, tiles.Count);
+        }
+        lastTileIndex = randomIndex;
+        return randomIndex;
 
     }
 }
